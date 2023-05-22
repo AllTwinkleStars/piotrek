a4e87a0ceeb288af5860ed084c04c2844f9a8fa2
AllTwinkleStars <KeepGrowinga@gmail.com>
2023-05-22 21:12:08 +0200

    text logo to uppercase

--- app/components/header/Logo.js
+++ app/components/header/Logo.js
@@ -4,12 +4,12 @@ export default function Logo(){
 
     return(
         <Link href="/">
-            <div className="text-xl w-55 tracking-widest">
-                <p className="font-light">KANCELARIA ADWOKACKA <br /> 
-                    <span className="font-bold">PIOTR TOMASZ WOŹNIAK</span>
+            <div className="text-xl uppercase w-55 tracking-widest">
+                <p className="font-light ">Kancelaria Adwokacka <br /> 
+                    <span className="font-bold">Piotr Tomasz Woźniak</span>
                 </p>
             </div>
-        </Link>
+        </Link> 
     )
 }
 
