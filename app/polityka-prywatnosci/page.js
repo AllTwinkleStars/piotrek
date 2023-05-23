ab68c7bc121ca4a8e0057b6db2265e8db9f01c58
AllTwinkleStars <KeepGrowinga@gmail.com>
2023-05-23 15:44:04 +0200

    fix mobile H1 style cd2.

--- app/polityka-prywatnosci/page.js
+++ app/polityka-prywatnosci/page.js
@@ -1,6 +1,6 @@
 export default function PolitykaPrywatnosciPage() {
     return (
-      <article className="prose prose-h1:text-2xl prose-h1:text-pw-gold prose-h1:mb-12 font-serif text-[18px] border-t-pw-gold border-t-4 pt-8">
+      <article className="pt-8 font-sans leading-8 prose border-t-4 prose-h1:text-2xl prose-h1:text-pw-gold prose-h1:mb-8 lg:prose-h1:mb-12 prose-a:text-pw-green prose-a:underline prose-a:decoration-1 hover:prose-a:decoration-2 prose-a:underline-offset-4 prose-a:decoration-pw-gold hover:prose-a:underline hover:prose-a:decoration-pw-gold text-md border-t-pw-gold">
         Hello from polityka prywatności
       </article>
     )
