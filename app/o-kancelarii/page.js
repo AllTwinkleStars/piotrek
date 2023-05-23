0401df82f5eafacde54ff5f6c5f12bbe61ccbbf4
AllTwinkleStars <KeepGrowinga@gmail.com>
2023-05-23 11:45:59 +0200

    add google maps API libs

--- app/o-kancelarii/page.js
+++ app/o-kancelarii/page.js
@@ -2,7 +2,7 @@ export const metadata = {
   title:  'O Kancelarii',
 };
 
-export default function OkancelariiPage() {
+export default function OKancelariiPage() {
     return (
       <article className="pt-8 font-sans leading-8 prose border-t-4 prose-h1:text-2xl prose-h1:text-pw-gold prose-h1:mb-12 text-md border-t-pw-gold">
       <h1 className="font-sans font-normal">O Kancelarii</h1>
