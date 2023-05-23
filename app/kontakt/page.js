2b9ddd01d94b1f1a54a4d194c0a2ce9334e603e4
AllTwinkleStars <KeepGrowinga@gmail.com>
2023-05-23 18:58:06 +0200

    no padding article on mobile2

--- app/kontakt/page.js
+++ app/kontakt/page.js
@@ -13,7 +13,7 @@ export default function KontaktPage() {
 
     return (
       <>
-        <article className="pt-8 font-sans leading-8 prose border-t-4 prose-h1:text-2xl prose-h1:text-pw-gold prose-h1:mb-8 lg:prose-h1:mb-12 prose-a:text-pw-green prose-a:underline prose-a:decoration-1 hover:prose-a:decoration-2 prose-a:underline-offset-4 prose-a:decoration-pw-gold hover:prose-a:underline hover:prose-a:decoration-pw-gold text-md border-t-pw-gold">
+        <article className="pt-0 font-sans leading-8 prose md:pt-8 md:border-t-4 prose-h1:text-2xl prose-h1:text-pw-gold prose-h1:mb-8 lg:prose-h1:mb-12 prose-a:text-pw-green prose-a:underline prose-a:decoration-1 hover:prose-a:decoration-2 prose-a:underline-offset-4 prose-a:decoration-pw-gold hover:prose-a:underline hover:prose-a:decoration-pw-gold text-md md:border-t-pw-gold">
           <h1 className="font-sans font-normal">Kontakt</h1>
           <p>ul. Kielecka 6, 31-516 Kraków<br />
           <a className="font-normal no-underline prose-a:no-underline text-inherit" href="tel:+48601177604">tel. +48 601 177 604</a><br />
--- app/specjalizacje/page.js
+++ app/specjalizacje/page.js
@@ -6,7 +6,7 @@ export const metadata = {
 
 export default function SpecjalizacjePage() {
     return (
-      <article className="pt-8 font-sans leading-8 prose border-t-4 prose-h1:text-2xl prose-h1:text-pw-gold prose-h1:mb-8 lg:prose-h1:mb-12 prose-a:text-pw-green-900 prose-a:no-underline hover:prose-a:decoration-1 prose-a:underline-offset-4 prose-a:decoration-pw-gold-300 hover:prose-a:text-pw-green-800 hover:prose-a:underline hover:prose-a:decoration-pw-gold text-md border-t-pw-gold">
+      <article className="pt-0 font-sans leading-8 prose md:pt-8 md:border-t-4 prose-h1:text-2xl prose-h1:text-pw-gold prose-h1:mb-8 lg:prose-h1:mb-12 prose-a:text-pw-green prose-a:underline prose-a:decoration-1 hover:prose-a:decoration-2 prose-a:underline-offset-4 prose-a:decoration-pw-gold hover:prose-a:underline hover:prose-a:decoration-pw-gold text-md md:border-t-pw-gold">
       <h1 className="font-sans font-normal">Specjalizacje</h1>
         <p>Kancelaria specjalizuje się w prowadzeniu spraw z zakresu:</p>
         <ul>
